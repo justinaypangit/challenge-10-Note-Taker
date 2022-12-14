553d8fdeb8a64081d841eb40178aca12fb907c4a
justinaypangit <just10gv@gmail.com>
2022-12-13 20:54:45 -0800

    Updated html and fixed routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,11 +2,11 @@ const path = require("path");
 const router = require("express").router();
 
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../assets/notes.html"));
+    res.sendFile(path.join(__dirname, "../assets/notes.html"));
 });
 
 router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../assets/index.html"));
+    res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 module.exports = router;
